f59662b09f44f41a823de3eb0ef344aba3b3d708
Patrick de Beer <p.debeer@fontys.nl>
2021-10-01 18:44:53 +0200

    project set-up

---
+++ src/main/java/com/ec/apis/easycontacts/EasyContactsApplication.java
@@ -0,0 +1,13 @@
+package com.ec.apis.easycontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyContactsApplication.class, args);
+    }
+
+}
