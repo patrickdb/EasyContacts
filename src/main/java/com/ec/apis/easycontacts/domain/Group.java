83052b2ded26310c7b844819b082e2cc97b86172
Patrick de Beer <p.debeer@fontys.nl>
2021-10-01 22:27:08 +0200

    Solve 406 issue for /groups

--- src/main/java/com/ec/apis/easycontacts/domain/Group.java
+++ src/main/java/com/ec/apis/easycontacts/domain/Group.java
@@ -5,4 +5,7 @@ import java.util.List;
 public class Group {
     private String name;
     private List<GroupMember> members;
+
+    public String getName() { return name; }
+    public List<GroupMember> getMembers() { return members; }
 }
